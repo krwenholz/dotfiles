b24017f78e71224ed9c78ee9d71cb61d0a892d09
Kyle R Wenholz <kyle@krwenholz.com>
2015-12-06 13:17:52 -0800

    Clean up Fedora setup

--- home/.zsh_helpers/Fedora.sh
+++ home/.zsh_helpers/Fedora.sh
@@ -4,26 +4,29 @@ mkdir $HOME/configs
 
 # Solarized for Gnome Terminal
 if [ ! -d $HOME/configs/gnome-terminal-colors-solarized ]; then
-  git clone https://github.com/Anthony25/gnome-terminal-colors-solarized $HOME/Downloads
-  $/HOME/configs/gnome-terminal-colors-solarized/install.sh
-  $/HOME/configs/gnome-terminal-colors-solarized/set_light.sh
+  git clone https://github.com/Anthony25/gnome-terminal-colors-solarized $HOME/configs/gnome-terminal-colors-solarized
+  $HOME/configs/gnome-terminal-colors-solarized/install.sh
 fi
 
 echo "Installing useful packages for Fedora"
-TO_INSTALL=""
+to_install=""
 if [ ! -f /usr/bin/vim ]; then
- TO_INSTALL=TO_INSTALL + " vim"
+  to_install=$to_install + " vim"
 fi
 if [ ! -f /usr/bin/nano ]; then
- TO_INSTALL=TO_INSTALL + " nano"
+  to_install=$to_install + " nano"
 fi
 if [ ! -f /usr/bin/tig ]; then
-  TO_INSTALL=TO_INSTALL + "tig"
+  to_install=$to_install + " tig"
+fi
+if [ ! -f /usr/bin/vim ]; then
+  to_install=$to_install + " vim-enhanced"
 fi
 
-if [ TO_INSTALL!="" ]; then
-  sudo dnf install TO_INSTALL
+if [ ! $to_install == "" ]; then
+  echo "Decided to install " $to_install
+  sudo dnf install $to_install
 fi
 
 export JAVA_HOME=/etc/alternatives/jre_openjdk
-export PATH=PATH:$HOME/bin/idea-IC-143.382.35/bin
+export PATH=$PATH:$HOME/bin/idea-IC-143.382.35/bin
