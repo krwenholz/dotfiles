9464f3f60ada9564303d1164757c48086bf4878b
Kyle Wenholz <kyle@krwenholz.com>
2016-02-09 08:22:16 -0800

    Add basic command line utilities to Ubuntu
    Just copied these over from Fedora, so no guarantees they wor

--- home/.zsh_helpers/Ubuntu.sh
+++ home/.zsh_helpers/Ubuntu.sh
@@ -4,3 +4,34 @@
 # tar -xzvf xflux64.tgz
 # now we have an executable, but it says it won't work on my display
 # cd ~
+
+echo "Configuring Ubuntu"
+# Configuration dir
+if [ ! -d $HOME/configs ]; then
+    mkdir $HOME/configs 
+fi
+
+# Solarized for Gnome Terminal
+if [ ! -d $HOME/configs/gnome-terminal-colors-solarized ]; then
+  git clone https://github.com/Anthony25/gnome-terminal-colors-solarized $HOME/configs/gnome-terminal-colors-solarized
+  $HOME/configs/gnome-terminal-colors-solarized/install.sh
+fi
+
+to_install=""
+if [ ! -f /usr/bin/vim ]; then
+  to_install=$to_install + " vim"
+fi
+if [ ! -f /usr/bin/nano ]; then
+  to_install=$to_install + " nano"
+fi
+if [ ! -f /usr/bin/tig ]; then
+  to_install=$to_install + " tig"
+fi
+if [ ! -f /usr/bin/vim ]; then
+  to_install=$to_install + " vim-enhanced"
+fi
+
+if [ "$to_install" -ne "" ]; then
+  echo "Decided to install " $to_install
+  sudo apt-get install $to_install
+fi
