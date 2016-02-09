2c586c156165cc883233fa75b164d4ea93e3915b
Kyle Wenholz <kyle@krwenholz.com>
2016-02-09 08:20:14 -0800

    Started an attempt to get f.lux on Ubuntu

---
+++ home/.zsh_helpers/Ubuntu.sh
@@ -0,0 +1,6 @@
+# check if f.lux is installed
+# cd ~/Downloads
+# wget https://justgetflux.com/linux/xflux64.tgz
+# tar -xzvf xflux64.tgz
+# now we have an executable, but it says it won't work on my display
+# cd ~
