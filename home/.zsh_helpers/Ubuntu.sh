3e6c3f327f5f62ffb1463db399bfb327d5396a41
Kyle R Wenholz <kyle@krwenholz.com>
2016-03-23 16:13:02 -0700

    Add Intellij desktop icon for ubuntu

--- home/.zsh_helpers/Ubuntu.sh
+++ home/.zsh_helpers/Ubuntu.sh
@@ -39,6 +39,21 @@ if [ ! -z "$to_install" ]; then
   sudo apt-get install $to_install
 fi
 
+if [ -f /usr/share/applications/intellij.desktop ]; then
+  installedIntellijIcon=`cat /usr/share/applications/intellij.desktop`
+else
+  installedIntellijIcon="foobar"
+fi
+myIntellijIcon=`cat $HOME/.to_link/intellij.desktop`
+if [[ "$installedIntellijIcon" != "$myIntellijIcon" ]]; then
+  echo "Installing Intellij desktop icon"
+  # setting up Ubuntu icon
+  sudo cp $HOME/.to_link/intellij.desktop /usr/share/applications/intellij.desktop
+  sudo sed -i "s!HOME!$HOME!g" /usr/share/applications/intellij.desktop
+  sudo chmod 644 /usr/share/applications/intellij.desktop
+  sudo update-desktop-database
+fi
+
 #######################################################################
 # Aliases
 ########################################################################
