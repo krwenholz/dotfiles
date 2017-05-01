0277ae10e2833202b9a58f96fb1dd08b6756c983
Kyle R Wenholz <kyle@krwenholz.com>
2017-04-30 22:18:16 -0700

    VS Code additions (TODOs really)

--- home/.zsh_helpers/Mac.sh
+++ home/.zsh_helpers/Mac.sh
@@ -1,4 +1,17 @@
 tigrc=`cat $HOME/.tigrc`
-if [[ $tigro != *"set vertical-split = no"* ]]; then
+if [[ $tigrc != *"set vertical-split = no"* ]]; then
   echo "set vertical-split = no" >> $HOME/.tigrc
+fi
+
+if [ ! -f /usr/local/bin/mono ]; then
+  echo "Installing mono for F#"
+  brew install mono
+fi
+
+if [ ! -d /Applications/Visual\ Studio\ Code.app/ ]; then
+  echo "TODO: we should install Visual Studio Code"
+fi
+
+if [ ! -f /Users/$USER/Library/Application Support/Code/User/settings.json ]; then
+  echo "TODO: Install VS Code settings"
 fi
