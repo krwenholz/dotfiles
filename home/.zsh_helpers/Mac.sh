01d246a16d9ca95dc9df4f55b6f18f37633b7315
Kyle R Wenholz <kyle@krwenholz.com>
2017-05-29 21:37:28 -0700

    Install tmux on mac and remove cruft

--- home/.zsh_helpers/Mac.sh
+++ home/.zsh_helpers/Mac.sh
@@ -3,15 +3,7 @@ if [[ $tigrc != *"set vertical-split = no"* ]]; then
   echo "set vertical-split = no" >> $HOME/.tigrc
 fi
 
-if [ ! -f /usr/local/bin/mono ]; then
-  echo "Installing mono for F#"
-  brew install mono
-fi
-
-if [ ! -d /Applications/Visual\ Studio\ Code.app/ ]; then
-  echo "TODO: we should install Visual Studio Code"
-fi
-
-if [ ! -f /Users/$USER/Library/Application Support/Code/User/settings.json ]; then
-  echo "TODO: Install VS Code settings"
+if [ ! -f /usr/local/bin/tmux ]; then
+  echo "Installing tmux"
+  brew install tmux
 fi
