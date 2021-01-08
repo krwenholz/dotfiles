fcc19841ae34e103592fe787bb38ffc5ab20cb5a
Kyle Wenholz <kyle@ngrok.com>
2021-01-08 14:03:42 -0800

    Add update and distribute scripts

---
+++ distribute.sh
@@ -0,0 +1,3 @@
+cp --recursive --force home/* $HOME
+sudo etc/nixos/configuration.nix /etc/nixos/configuration.nix
+echo "Don't forget to rebuild nix for changes to take effect"
---
+++ update.sh
@@ -0,0 +1,4 @@
+cp $HOME/*.nix home
+cp --recursive $HOME/includes home
+git add .
+git commit
