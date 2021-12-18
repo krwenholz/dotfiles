e55fb77efa97352e87e6880a850995bbb04eec10
Kyle Wenholz <kyle@ngrok.com>
2021-12-18 15:09:41 -0800

    Fix vim colors?

--- home/shell.nix
+++ home/shell.nix
@@ -22,6 +22,7 @@
       keyMode = "vi";
         #bind-key -T copy-mode y send-keys -X copy-pipe-and-cancel 'xclip -selection clipboard >/dev/null'
       extraConfig = ''
+        set -g default-terminal "xterm-256color"
         new-session
         bind P paste-buffer
         bind-key -T copy-mode-vi v send-keys -X begin-selection
