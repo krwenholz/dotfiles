3ddeb1ab67588a51da0f4356086b9f3f15ebdab6
Kyle Wenholz <kyle@ngrok.com>
2022-12-23 06:29:13 -0800

    Add leap-nvim config

--- home/includes/vim/config.lua
+++ home/includes/vim/config.lua
@@ -200,7 +200,7 @@ local lsp_defaults = {
 	flags = {
 		debounce_text_changes = 150,
 	},
-	capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
 	on_attach = function()
 		vim.api.nvim_exec_autocmds("User", { pattern = "LspAttached" })
 	end,
@@ -402,6 +402,12 @@ luasnip.add_snippets("all", {
 		luasnip.text_node(os.date("%Y-%m-%d")),
 	}),
 })
+
+------------------------------------------------------------------
+-- leap.nvim
+------------------------------------------------------------------
+
+require("leap").add_default_mappings()
 
 ------------------------------------------------------------------
 -- Custom functions
