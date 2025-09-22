bd651267d573a6732fc3c05d1dcbe98ac229c784
Kyle Wenholz <kyle@krwenholz.com>
2025-09-22 17:10:16 +0000

    Add tmux

---
+++ dot_config/tmux-powerline/config.sh
@@ -0,0 +1,5 @@
+# ~/.local/share/chezmoi/dot_config/tmux-powerline/config.sh
+
+export TMUX_POWERLINE_THEME="default"
+export TMUX_POWERLINE_DEBUG_MODE_ENABLED="false"
+export TMUX_POWERLINE_PATCHED_FONT_IN_USE="true"
