a865699e5f300543416d1fea97fc26cdf23aaa7e
Kyle Wenholz <kyle@krwenholz.com>
2025-12-16 22:35:53 +0000

    Add default claude mcp servers

---
+++ run_onchange_install-claude-code.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+claude mcp add --transport http sentry https://mcp.sentry.dev/mcp --scope user
+claude mcp add --transport http figma https://mcp.figma.com/mcp --scope user
+claude mcp add --transport http github https://api.githubcopilot.com/mcp -H "Authorization: Bearer YOUR_GITHUB_PA" --scope user
