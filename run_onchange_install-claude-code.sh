2093736a287a4d809ca50f6f25e72ab1e3f38787
Kyle Wenholz <kyle@krwenholz.com>
2026-01-02 16:33:58 +0000

    Don't fail claude mcp server installs

--- run_onchange_install-claude-code.sh
+++ run_onchange_install-claude-code.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-claude mcp add --transport http sentry https://mcp.sentry.dev/mcp --scope user
-claude mcp add --transport http figma https://mcp.figma.com/mcp --scope user
-claude mcp add --transport http github https://api.githubcopilot.com/mcp -H "Authorization: Bearer YOUR_GITHUB_PA" --scope user
+claude mcp add --transport http sentry https://mcp.sentry.dev/mcp --scope user || true
+claude mcp add --transport http figma https://mcp.figma.com/mcp --scope user || true
+claude mcp add --transport http github https://api.githubcopilot.com/mcp -H "Authorization: Bearer YOUR_GITHUB_PA" --scope user || true
