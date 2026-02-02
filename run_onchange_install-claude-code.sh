322a010cf22e4639355eb3c2fbc552430f497a96
Kyle Wenholz <kyle@krwenholz.com>
2026-02-02 23:38:48 +0000

    Add Notion MCP

--- run_onchange_install-claude-code.sh
+++ run_onchange_install-claude-code.sh
@@ -2,3 +2,4 @@
 claude mcp add --transport http sentry https://mcp.sentry.dev/mcp --scope user || true
 claude mcp add --transport http figma https://mcp.figma.com/mcp --scope user || true
 claude mcp add --transport http github https://api.githubcopilot.com/mcp -H "Authorization: Bearer YOUR_GITHUB_PA" --scope user || true
+claude mcp add --transport http notion https://mcp.notion.com/mcp --scope user || true
