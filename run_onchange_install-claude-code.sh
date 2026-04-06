792bf565600597578022637f308c677cabef0600
Kyle Wenholz <kyle@krwenholz.com>
2026-04-06 17:01:30 +0000

    Remove manual mcp installs

--- run_onchange_install-claude-code.sh
+++ run_onchange_install-claude-code.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-claude mcp add --transport http sentry https://mcp.sentry.dev/mcp --scope user || true
-claude mcp add --transport http figma https://mcp.figma.com/mcp --scope user || true
-claude mcp add --transport http github https://api.githubcopilot.com/mcp -H "Authorization: Bearer YOUR_GITHUB_PA" --scope user || true
-claude mcp add --transport http notion https://mcp.notion.com/mcp --scope user || true
+#claude mcp add --transport http sentry https://mcp.sentry.dev/mcp --scope user || true
+#claude mcp add --transport http figma https://mcp.figma.com/mcp --scope user || true
+#claude mcp add --transport http github https://api.githubcopilot.com/mcp -H "Authorization: Bearer YOUR_GITHUB_PA" --scope user || true
+#claude mcp add --transport http notion https://mcp.notion.com/mcp --scope user || true
 claude plugin install jj-git-guard@fay-plugin-marketplace --scope user || true
